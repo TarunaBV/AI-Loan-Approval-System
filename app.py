fd8df1a28b396e34a03e2d7c2ea70c8c3c5c1587
Taruna BV <tarunabvtarun@gmail.com>
2026-06-09 19:18:53 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "AI Loan Approval System Running Successfully!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
