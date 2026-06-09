78fdaa50ea0a036d182b5214f97e1c2f845161cb
Taruna BV <tarunabvtarun@gmail.com>
2026-06-09 19:14:03 +0530

    Updated app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "AI Loan Approval System Running Successfully!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
