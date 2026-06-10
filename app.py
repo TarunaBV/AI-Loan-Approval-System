7993e6ac3884199801d4ce989ba30046ee665d2e
Taruna BV <tarunabvtarun@gmail.com>
2026-06-10 20:21:16 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,18 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
-model = joblib.load('model/loan_model.pkl')
+model = joblib.load("model/loan_model.pkl")
+encoders = joblib.load("model/encoders.pkl")
 
-encoders = joblib.load('model/encoders.pkl')
-
-print("Model Features:")
-print(model.feature_names_in_)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    data = {
+        "Gender": request.form["Gender"],
+        "Married": request.form["Married"],
+        "Dependents": request.form["Dependents"],
+        "Education": request.form["Education"],
+        "Self_Employed": request.form["Self_Employed"],
+        "ApplicantIncome": float(request.form["ApplicantIncome"]),
+        "CoapplicantIncome": float(request.form["CoapplicantIncome"]),
+        "LoanAmount": float(request.form["LoanAmount"]),
+        "Loan_Amount_Term": float(request.form["Loan_Amount_Term"]),
+        "Credit_History": float(request.form["Credit_History"]),
+        "Property_Area": request.form["Property_Area"]
+    }
+
+    df = pd.DataFrame([data])
+
+    for column in encoders:
+
+        if column == "Loan_Status":
+            continue
+
+        df[column] = encoders[column].transform(df[column])
+
+    prediction = model.predict(df)[0]
+
+    if prediction == 1:
+        result = "✅ Loan Approved"
+    else:
+        result = "❌ Loan Rejected"
+
+    return render_template("result.html", result=result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
