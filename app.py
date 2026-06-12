f3dd484e03b6dbd380805e73a693de03995e9815
Taruna BV <tarunabvtarun@gmail.com>
2026-06-12 16:26:19 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
 import joblib
 import pandas as pd
+import mysql.connector
 
 app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Taru..KA.15",
+    database="loan_approval_db"
+)
+
+cursor = db.cursor()
 
 model = joblib.load("model/loan_model.pkl")
 encoders = joblib.load("model/encoders.pkl")
@@ -40,11 +50,49 @@ def predict():
         df[column] = encoders[column].transform(df[column])
 
     prediction = model.predict(df)[0]
+    prediction_text = "Approved" if prediction == 1 else "Rejected"
 
-    if prediction == 1:
-        result = "✅ Loan Approved"
-    else:
-        result = "❌ Loan Rejected"
+    result = (
+        "✅ Loan Approved"
+        if prediction == 1
+        else "❌ Loan Rejected"
+    )
+    
+    query = """
+    INSERT INTO loan_applications(
+        gender,
+        married,
+        dependents,
+        education,
+        self_employed,
+        applicant_income,
+        coapplicant_income,
+        loan_amount,
+        loan_term,
+        credit_history,
+        property_area,
+        prediction
+    )
+    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    """
+
+    values = (
+        data["Gender"],
+        data["Married"],
+        data["Dependents"],
+        data["Education"],
+        data["Self_Employed"],
+        data["ApplicantIncome"],
+        data["CoapplicantIncome"],
+        data["LoanAmount"],
+        data["Loan_Amount_Term"],
+        data["Credit_History"],
+        data["Property_Area"],
+        prediction_text
+    )
+
+    cursor.execute(query, values)
+    db.commit()
 
     return render_template("result.html", result=result)
 
